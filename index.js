05dc874e261908ffa85712fcbbdbb9204174d863
Abhishek Yelley <yabhishek.19.cse@anits.edu.in>
2023-12-20 21:59:12 +0530

    Added a special error when incorrect parameters are sent

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ const app = express();
 const { getDetailedData, getSmallData } = require('./scraper')
 app.use(cors())
 
+const missingParamsError = {
+    error: true,
+    message: "Missing required params (uid and/or fid)",
+    status: 422
+}
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h3>visit "/review?uid=USER_ID&fid=FILM_ID&vid=REVIEW_VERSION" for short review and "/review-detailed" for the long one</h3>')
+    res.status(200).send(`
+    <body style="background-color: black">
+        <p style="color:white">
+            visit
+            <br>
+            "/review?uid=USER_ID&fid=FILM_ID&vid=REVIEW_VERSION" for short review
+            <br>
+            and
+            <br>
+            "/review-detailed" for the long one
+        </p>
+    </body>
+    
+    `)
 })
 app.get('/review', (req, res) => {
     const uid = req.query.uid
     const fid = req.query.fid
     const vid = req.query.vid
+    if(!uid || !fid){
+        return res.status(missingParamsError.status).json(missingParamsError)
+    }
     getSmallData(uid, fid, vid)
     .then((details) => {
         res.json(details)
@@ -24,6 +45,10 @@ app.get('/review-detailed', (req, res) => {
     const uid = req.query.uid
     const fid = req.query.fid
     const vid = req.query.vid
+    if(!uid || !fid){
+        return res.status(missingParamsError.status).json(missingParamsError)
+        
+    }
     getDetailedData(uid, fid, vid)
     .then((details) => {
         res.json(details)
